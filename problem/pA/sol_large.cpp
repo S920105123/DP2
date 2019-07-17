716d198830e77e24c8c7d5ce3d2d205598fa1b20
Benjamin <milk500ted939@gmail.com>
2019-07-17 18:24:09 +0800

    Do pA

--- problem/pA/sol_large.cpp
+++ problem/pA/sol_large.cpp
@@ -1,39 +1,85 @@
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
 #define LL long long
+#define PII pair<int, int> 
 const int INF = (int)1e9 + 5;
 const int MAXN = 100005;
+const int MAXC = 100005;
 using namespace std;
 
 typedef vector<int> vec;
 typedef vector<vec> mat;
 
-int N, M, strat_r[MAXN], strat_d1[2*MAXN], strat_d2[2*MAXN];
-mat w, p;
-vector<pair<int, int>> ord;
+int N, M, from_r[MAXN], from_d1[2 * MAXN], from_d2[2 * MAXN];
+mat H, P, dp;
+vector<PII> rnk[MAXC];
+
+int run(int _N, int _M, PII src, PII tgt, mat _H, mat _P) {
+	/* Copy the parameters for convenience of testing. */
+	N = _N;
+	M = _M;
+	H = move(_H);
+	P = move(_P);
+	dp = mat(N, vec(M, -INF));
+	fill(from_r, from_r + MAXN, -INF);
+	fill(from_d1, from_d1 + 2 * MAXN, -INF);
+	fill(from_d2, from_d2 + 2 * MAXN, -INF);
+	
+	/* Do dp[i][j]: Maximum profit to reach here from (rs, cs). */
+	for (int i = 0; i < MAXC; i++) {
+		for (PII coord : rnk[i]) {
+			int r = coord.first, c = coord.second;
+			if (coord == src) {
+				dp[r][c] = 0;
+			}
+			else {
+				dp[r][c] = max({from_r[r], from_d1[r + c], from_d2[r - c + (M - 1)]});
+			}
+			
+			if (dp[r][c] != -INF) {
+				dp[r][c] += P[r][c];
+			}
+		}
+		for (PII coord : rnk[i]) {
+			int r = coord.first, c = coord.second;
+			from_r[r] = max(from_r[r], dp[r][c]);
+			from_d1[r + c] = max(from_d1[r + c], dp[r][c]);
+			from_d2[r - c + (M - 1)] = max(from_d2[r - c + (M - 1)], dp[r][c]);
+		}
+	}
+	
+	return dp[tgt.first][tgt.second];
+}
 
 int main() {
 	// ios_base::sync_with_stdio(0); cin.tie(0);
 	
-	cin >> N >> M;
-	w = mat(N, vec(M));
-	p = mat(N, vec(M));
+	int rs, cs, rt, ct;
+	cin >> N >> M >> rs >> cs >> rt >> ct;
+	H = mat(N, vec(M));
+	P = mat(N, vec(M));
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			cin >> w[i][j];
-			ord.push_back({w[i][j], i * M + j});
+			cin >> H[i][j];
+			rnk[H[i][j]].push_back({i, j});
 		}
 	}
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			cin >> p[i][j];
+			cin >> P[i][j];
 		}
 	}
 	
-	sort(ord.begin(), ord.end());
-	for (pair<int, int> p : ord) {
-		
+	int result = run(N, M, PII(rs, cs), PII(rt, ct), H, P);
+	if (result == -INF) {
+		cout << "You are fired!\n";
 	}
-	
+	else {
+		cout << result << '\n';
+	}
 	
 	return 0;
 }
+
